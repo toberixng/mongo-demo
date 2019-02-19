2e143ae9e190a74cea3dcc0f0b15fd9cd8bf9ab4
toberixng <tobyfatumo@gmail.com>
2019-02-19 19:41:25 +0100

    first update

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ const courseSchema = new mongoose.Schema({
         type: Array,
         validate: {
             validator: function(v){
-                return v.length >= 0;
+                return v && v.length > 0;
             },
-            message: 'A course should have at least on tag'
+            message: 'A course should have at least on tag.'
         }
     },
     date: { type: Date, default: Date.now },
@@ -42,7 +42,7 @@ const Course = mongoose.model('Course', courseSchema);
 async function createCourse() {
     const course = new Course({
         name: 'Node.js Course',
-        category: '-',
+        category: 'web',
         author: 'Toberixng',
         tags: [],            
         isPublished: true,
